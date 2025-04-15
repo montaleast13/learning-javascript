9abdd8e30b59b77afad6423740ea1629f7381418
unknown <monazzatalha@hotmail.com>
2025-04-15 11:55:37 -0700

    this is my variables javascript file

---
+++ variables.js
@@ -0,0 +1,20 @@
+// NUMBERS
+
+let x = 5
+console.log(x)
+
+let y = 5.5
+console.log(y)
+
+// STRINGS
+
+let z = "hello"
+console.log(z)
+
+let a = "this is my string sentence"
+console.log(a)
+
+// BOOLEAN
+
+let b = true
+console.log(b)
